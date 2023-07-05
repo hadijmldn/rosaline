53156eb81da349fc9495527301050a04757d074d
Hadi Jamaludin <hadijmldn@gmail.com>
2023-07-05 14:30:52 +0700

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,25 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY Love!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY Rosaline!</h3>
+        <p>Dear My beloved Girlfrined,</p>
+        <p>
+          Happy birthday!! I hope your day is filled with lots of love and
+          laughter! May all of your birthday wishes come true.
+        </p>
+        <div className="name">
+        <p className="name">Hadi</p>
+        </div>
+      </div>
+    </div>
+  );
+  
